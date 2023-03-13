11cec482a70119307c2ce7d74f4ec181db84c89b
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-13 23:30:31 +0300

    feat: add routing

--- rs-react/src/App.test.tsx
+++ rs-react/src/App.test.tsx
@@ -1,12 +1,13 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('Renders hello world', () => {
     // ARRANGE
-    render(<App />);
+    render(<WrappedApp />);
     // Act
     // Expect
     expect(
@@ -14,5 +15,17 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('Hello World');
+  });
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/banana']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
   });
 });
