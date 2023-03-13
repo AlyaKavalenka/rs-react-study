23ba4b35dce8cda1c9246ddee8f2d792168a3068
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-13 22:46:19 +0300

    feat: add initial test for test testing))

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -1,15 +1,5 @@
 function App() {
-  return (
-    <form>
-      <input
-        type="text"
-        id="dinosaur_name"
-        name="dinosaur_name"
-        aria-describedby="dinosaur_name_info"
-        className="dinosaur_name"
-      />
-    </form>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
