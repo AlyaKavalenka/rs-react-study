c857b9b27b4cc6654101050d5eb31a9d5409dbc9
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-15 03:28:12 +0300

    feat: add about us page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AboutUs from './pages/AboutUs/AboutUs';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 
@@ -8,6 +9,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="about" element={<AboutUs />} />
       </Routes>
     </div>
   );
