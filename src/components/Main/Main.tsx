1c8ebf94d41b575103366d443ae8a93029ce8d95
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-16 17:34:23 +0300

    feat: add main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <main className="main">
+      <input type="text" />
+      <div>Cards</div>
+    </main>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import './Home.scss';
 import Header from '../../components/Header/Header';
+import Main from '../../components/Main/Main';
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
+      <Main />
     </div>
   );
 }
