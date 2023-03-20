29d6c0613ed3663eb48ac8c693000cc550427064
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-20 14:19:23 +0300

    feat: add form route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import AboutUs from './pages/AboutUs/AboutUs';
+import Form from './pages/Form/Form';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 
@@ -12,6 +13,7 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="about" element={<AboutUs />} />
+        <Route path="form" element={<Form />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Form/Form.tsx
@@ -0,0 +1,9 @@
+export default function Form() {
+  return (
+    <main>
+      <div className="form">
+        <h1>Form</h1>
+      </div>
+    </main>
+  );
+}
