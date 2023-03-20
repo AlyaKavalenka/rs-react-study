e5b8b2d6ce57dee26fe1f7e7bd3eeb24317f0eb8
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-20 14:34:34 +0300

    feat: add test for success render about us

--- src/pages/AboutUs/AboutUs.test.tsx
+++ src/pages/AboutUs/AboutUs.test.tsx
@@ -4,6 +4,11 @@ import { render, screen } from '@testing-library/react';
 import AboutUs from './AboutUs';
 
 describe('AboutUs', () => {
+  it('renders without errors', () => {
+    const wrapper = render(<AboutUs />);
+    expect(wrapper).toBeTruthy();
+  });
+
   it('Renders not found about us header', () => {
     render(<AboutUs />);
     expect(
