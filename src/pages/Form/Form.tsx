f39ba5ad291997a591ee40643c8a540e280c5329
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-20 16:05:49 +0300

    feat: add basic form

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -2,7 +2,20 @@ export default function Form() {
   return (
     <main>
       <div className="form">
-        <h1>Form</h1>
+        <h2>Form</h2>
+        <form>
+          <input type="text" name="" id="" />
+          <input type="date" name="" id="" />
+          <label htmlFor="select">
+            <select name="select" id="select">
+              <option value="puck">Puck</option>
+            </select>
+          </label>
+          <input type="checkbox" />
+          <input type="radio" name="" id="" />
+          <input type="file" name="" id="" />
+          <input type="submit" value="" />
+        </form>
       </div>
     </main>
   );
