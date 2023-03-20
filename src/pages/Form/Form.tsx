260bccc5e42a1d6e853788f515754ef617c60323
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-20 16:58:20 +0300

    feat: wip form basic inputs

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,21 +1,38 @@
 import './Form.scss';
+import data from '../../assets/data/data.json';
 
 export default function Form() {
+  const array = data.products;
+  const categArr = array.map((item) => item.category);
+  const filteredCategArr = categArr.filter(
+    (item, index) => categArr.indexOf(item) === index
+  );
+  const optionArr = filteredCategArr.map((item) => (
+    <option value={item} key={`${item}`}>
+      {item}
+    </option>
+  ));
   return (
     <main>
       <div className="form-container">
         <form className="form">
-          <input type="text" name="" id="" />
+          <input type="text" placeholder="Type product name" />
+          <input type="text" placeholder="Type product description" />
+          <input type="text" placeholder="Type product price" />
           <input type="date" name="" id="" />
           <label htmlFor="select">
+            Select category
             <select name="select" id="select">
-              <option value="puck">Puck</option>
+              {optionArr}
             </select>
           </label>
-          <input type="checkbox" />
           <input type="radio" name="" id="" />
           <input type="file" name="" id="" />
-          <input type="submit" value="" />
+          <label htmlFor="checkbox">
+            <input type="checkbox" id="checkbox" />
+            Agree to terms and conditions
+          </label>
+          <input type="submit" value="Submit" />
         </form>
       </div>
     </main>
