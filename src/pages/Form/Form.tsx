692c259d1d16d86ae7101a1556b332fd73ea6eee
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-03-25 16:59:45 +0300

    feat: add style for form

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -33,76 +33,194 @@ export default function Form() {
 
   return (
     <main>
-      <div className="form-container">
-        <form className="form">
-          <input
-            type="text"
-            placeholder="Type product name"
-            onInput={(e) => setName(e.currentTarget.value)}
-          />
-          <input
-            type="text"
-            placeholder="Type product description"
-            onInput={(e) => setDescr(e.currentTarget.value)}
-          />
-          <input
-            type="number"
-            placeholder="Type product price"
-            onInput={(e) => setPrice(+e.currentTarget.value)}
-          />
-          <input
-            type="date"
-            name=""
-            id=""
-            defaultValue={`${new Date().toISOString().split('T')[0]}`}
-            max={`${new Date().toISOString().split('T')[0]}`}
-            onChange={(e) => setDate(new Date(e.currentTarget.value))}
-          />
-          <label htmlFor="select">
-            Select category
-            <select
-              name="select"
-              id="select"
-              onChange={(e) => setCategory(e.currentTarget.value)}
-            >
-              {optionArr}
-            </select>
-          </label>
-          <div>
+      <div className="form-n-cards">
+        <div className="form-n-example">
+          <form className="form">
             <input
-              type="radio"
-              name="order"
-              id="inStock"
-              defaultChecked
-              value="In stock"
-              onChange={(e) => setOrder(e?.target.value)}
+              type="text"
+              placeholder="Type product name"
+              onInput={(e) => setName(e.currentTarget.value)}
+              className="form__input"
             />
-            <label htmlFor="inStock">In stock</label>
-
             <input
-              type="radio"
-              name="order"
-              id="underOrder"
-              value="Under the order"
-              onChange={(e) => setOrder(e?.target.value)}
+              type="text"
+              placeholder="Type product description"
+              onInput={(e) => setDescr(e.currentTarget.value)}
+              className="form__input"
             />
-            <label htmlFor="underOrder">Under the order</label>
-          </div>
-          <input
-            type="file"
-            name=""
-            id=""
-            accept="image/*"
-            onChange={(e) => readFile(e.target)}
-          />
-          <label htmlFor="checkbox">
-            <input type="checkbox" id="checkbox" />
-            Agree to terms and conditions
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <article>
-          <h3>Card Example</h3>
+            <input
+              type="number"
+              placeholder="Type product price"
+              onInput={(e) => setPrice(+e.currentTarget.value)}
+              className="form__input"
+            />
+            <input
+              type="date"
+              name=""
+              id=""
+              defaultValue={`${new Date().toISOString().split('T')[0]}`}
+              max={`${new Date().toISOString().split('T')[0]}`}
+              onChange={(e) => setDate(new Date(e.currentTarget.value))}
+              className="form__input"
+            />
+            <label htmlFor="select" className="select-block">
+              Select category
+              <select
+                name="select"
+                id="select"
+                onChange={(e) => setCategory(e.currentTarget.value)}
+                className="form__input_select"
+              >
+                {optionArr}
+              </select>
+            </label>
+            <div className="radio-block">
+              <div className="radio-block__input-wrapper">
+                <input
+                  type="radio"
+                  name="order"
+                  id="inStock"
+                  defaultChecked
+                  value="In stock"
+                  onChange={(e) => setOrder(e?.target.value)}
+                  className="radio-block__input"
+                />
+                <label htmlFor="inStock" className="radio-block__label">
+                  In stock
+                </label>
+              </div>
+              <div className="radio-block__input-wrapper">
+                <input
+                  type="radio"
+                  name="order"
+                  id="underOrder"
+                  value="Under the order"
+                  onChange={(e) => setOrder(e?.target.value)}
+                  className="radio-block__input"
+                />
+                <label htmlFor="underOrder" className="radio-block__label">
+                  Under the order
+                </label>
+              </div>
+            </div>
+            <div className="input-file-block">
+              <input
+                type="file"
+                name=""
+                id=""
+                accept="image/*"
+                onChange={(e) => readFile(e.target)}
+                className="input-file-block__input"
+              />
+              <span className="input-file-block__text">Choose file</span>
+            </div>
+            <label htmlFor="checkbox">
+              <input type="checkbox" id="checkbox" />
+              Agree to terms and conditions
+            </label>
+            <input
+              type="submit"
+              value="Create card"
+              className="form__submit-btn"
+            />
+          </form>
+          <article>
+            <h3>Card Example</h3>
+            <div className="cards">
+              <section className="card__wrapper">
+                <Card
+                  id={data.total + 1}
+                  name={name}
+                  description={descr}
+                  price={price}
+                  popularity={0}
+                  stock={0}
+                  animeName=""
+                  category={category}
+                  images={[image]}
+                  date={date}
+                  order={order}
+                />
+              </section>
+            </div>
+          </article>
+        </div>
+        <article className="cards">
+          <section className="card__wrapper">
+            <Card
+              id={data.total + 1}
+              name={name}
+              description={descr}
+              price={price}
+              popularity={0}
+              stock={0}
+              animeName=""
+              category={category}
+              images={[image]}
+              date={date}
+              order={order}
+            />
+          </section>
+          <section className="card__wrapper">
+            <Card
+              id={data.total + 1}
+              name={name}
+              description={descr}
+              price={price}
+              popularity={0}
+              stock={0}
+              animeName=""
+              category={category}
+              images={[image]}
+              date={date}
+              order={order}
+            />
+          </section>
+          <section className="card__wrapper">
+            <Card
+              id={data.total + 1}
+              name={name}
+              description={descr}
+              price={price}
+              popularity={0}
+              stock={0}
+              animeName=""
+              category={category}
+              images={[image]}
+              date={date}
+              order={order}
+            />
+          </section>
+          <section className="card__wrapper">
+            <Card
+              id={data.total + 1}
+              name={name}
+              description={descr}
+              price={price}
+              popularity={0}
+              stock={0}
+              animeName=""
+              category={category}
+              images={[image]}
+              date={date}
+              order={order}
+            />
+          </section>
+          <section className="card__wrapper">
+            <Card
+              id={data.total + 1}
+              name={name}
+              description={descr}
+              price={price}
+              popularity={0}
+              stock={0}
+              animeName=""
+              category={category}
+              images={[image]}
+              date={date}
+              order={order}
+            />
+          </section>
           <section className="card__wrapper">
             <Card
               id={data.total + 1}
