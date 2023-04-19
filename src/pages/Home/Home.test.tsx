54467cd27ff95148b31713b99ba4d1e819e7c673
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-19 17:16:36 +0300

    refactor: home test

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Home from './Home';
+import { store } from '../../Store/store';
 
 describe('Home', () => {
   it('renders without errors', () => {
-    const wrapper = render(<Home />);
+    const wrapper = render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
     expect(wrapper).toBeTruthy();
   });
 });
