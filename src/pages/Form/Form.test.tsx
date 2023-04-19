c96ea647f5bfd7243b21f588295ffada2e6e3bcd
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-19 17:17:46 +0300

    refactor: form test

--- src/pages/Form/Form.test.tsx
+++ src/pages/Form/Form.test.tsx
@@ -1,10 +1,16 @@
 import { describe, test, expect } from 'vitest';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Form from './Form';
+import { store } from '../../Store/store';
 
 describe('From', () => {
   test('renders without errors', () => {
-    const wrapper = render(<Form />);
+    const wrapper = render(
+      <Provider store={store}>
+        <Form />
+      </Provider>
+    );
     expect(wrapper).toBeTruthy();
   });
 });
